6a478534c59a44eccd434a30baf6547f8d1acd93
cheopsrule <blake.heyman@gmail.com>
2022-04-04 17:43:58 -0400

    Prettier

--- js/visualization.js
+++ js/visualization.js
@@ -22,10 +22,9 @@ d3.csv("data/data_bechdel_new - data_bechdel.csv").then((data) => {
   let stackFormatted;
   let stacked;
 
-  window.barChart = function(minYear, maxYear) {
-
+  window.barChart = function (minYear, maxYear) {
     // Clear existing bar chart
-    svg3.selectAll('*').remove();
+    svg3.selectAll("*").remove();
 
     // Create map of counts
     genreCounts = new Map();
@@ -58,86 +57,86 @@ d3.csv("data/data_bechdel_new - data_bechdel.csv").then((data) => {
 
     stacked = d3.stack().keys(["0", "1", "2", "3"])(stackFormatted);
 
-     // We will need scales for all of the following charts to be global
-  let x3, y3;
+    // We will need scales for all of the following charts to be global
+    let x3, y3;
 
-  // Barchart with counts of different species
+    // Barchart with counts of different species
 
-  let xKey3 = "Genres";
-  let yKey3 = "Count";
+    let xKey3 = "Genres";
+    let yKey3 = "Count";
 
-  // Create X scale
-  x3 = d3
-    .scaleBand()
-    .domain(d3.range(genreCounts.size))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+    // Create X scale
+    x3 = d3
+      .scaleBand()
+      .domain(d3.range(genreCounts.size))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
 
-  // Add x axis
-  svg3
-    .append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(
-      d3.axisBottom(x3).tickFormat((d) => Array.from(genreCounts.keys())[d])
-    )
-    .attr("font-size", "14px")
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", width - margin.right)
-        .attr("y", margin.bottom - 4)
-        .attr("fill", "black")
-        .attr("text-anchor", "end")
-        .text(xKey3)
-    );
+    // Add x axis
+    svg3
+      .append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(
+        d3.axisBottom(x3).tickFormat((d) => Array.from(genreCounts.keys())[d])
+      )
+      .attr("font-size", "14px")
+      .call((g) =>
+        g
+          .append("text")
+          .attr("x", width - margin.right)
+          .attr("y", margin.bottom - 4)
+          .attr("fill", "black")
+          .attr("text-anchor", "end")
+          .text(xKey3)
+      );
 
-  // Find max y
-  let maxY3 = d3.max(genreCounts, (d) => {
-    const valuesArray = Array.from(d[1].values());
-    return d3.sum(valuesArray);
-  });
-  
-  // Create Y scale
-  y3 = d3
-    .scaleLinear()
-    .domain([0, maxY3])
-    .range([height - margin.bottom, margin.top]);
+    // Find max y
+    let maxY3 = d3.max(genreCounts, (d) => {
+      const valuesArray = Array.from(d[1].values());
+      return d3.sum(valuesArray);
+    });
 
-  //    Add y axis
-  svg3
-    .append("g")
-    .attr("transform", `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y3))
-    .attr("font-size", "20px")
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", 0)
-        .attr("y", margin.top)
-        .attr("fill", "black")
-        .attr("text-anchor", "end")
-        .text(yKey3)
-    );
+    // Create Y scale
+    y3 = d3
+      .scaleLinear()
+      .domain([0, maxY3])
+      .range([height - margin.bottom, margin.top]);
 
-  // Show the bars
-  svg3
-    .append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stacked)
-    .enter()
-    .append("g")
-    .attr("fill", (d) => color(d.key))
-    .selectAll("rect")
-    // enter a second time = loop subgroup per subgroup to add all rectangles
-    .data((d) => d)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => x3(i))
-    .attr("y", (d) => y3(d[1]))
-    .attr("height", (d) => y3(d[0]) - y3(d[1]))
-    .attr("width", x3.bandwidth());
-  }
+    //    Add y axis
+    svg3
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y3))
+      .attr("font-size", "20px")
+      .call((g) =>
+        g
+          .append("text")
+          .attr("x", 0)
+          .attr("y", margin.top)
+          .attr("fill", "black")
+          .attr("text-anchor", "end")
+          .text(yKey3)
+      );
+
+    // Show the bars
+    svg3
+      .append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stacked)
+      .enter()
+      .append("g")
+      .attr("fill", (d) => color(d.key))
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data((d) => d)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x3(i))
+      .attr("y", (d) => y3(d[1]))
+      .attr("height", (d) => y3(d[0]) - y3(d[1]))
+      .attr("width", x3.bandwidth());
+  };
 
   barChart();
 });
