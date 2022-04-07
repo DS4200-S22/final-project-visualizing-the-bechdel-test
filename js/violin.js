b0a0afe72b8db434ce008b8a1ec9301a6fa195e5
cheopsrule <blake.heyman@gmail.com>
2022-04-07 13:21:59 -0400

    Box plot

--- js/violin.js
+++ js/violin.js
@@ -1,13 +1,16 @@
-// https:d3-graph-gallery.com/graph/violin_basicHist.html
+// https://d3-graph-gallery.com/graph/violin_basicHist.html
+
+// https://d3-graph-gallery.com/graph/boxplot_basic.html
 
 // set the dimensions and margins of the graph
 const violinMargin = { top: 10, right: 40, bottom: 30, left: 40 };
 const violinWidth = 800;
 const violinHeight = 400;
 
-// Axis titles
-const xTitle = "Bechdel Test Success";
-const yTitle = "IMDb Rating";
+const titleMap = {
+  imdb_rating: "IMDb Rating",
+  runtime_minutes: "Runtime (minutes)",
+};
 
 // Append the svg object to the body of the page
 const svg = d3
@@ -25,15 +28,27 @@ d3.csv("data/data_bechdel_newer.csv").then((data) => {
   let sumstat;
   let sortedPass;
   let sortedFail;
+  let selectedAttribute = "imdb_rating";
 
-  window.violinPlot = function (minYear, maxYear) {
+  window.violinPlot = function (minYear, maxYear, attribute) {
     // Clear existing violin plot
     svg.selectAll("*").remove();
 
+    // Set selected attribute
+    selectedAttribute = attribute ?? selectedAttribute;
+
+    // Axis titles
+    const xTitle = "Bechdel Test Success";
+    const yTitle = titleMap[selectedAttribute];
+
     // Build and Show the Y scale
+    let maxY = d3.max(data, (d) => Number.parseInt(d[selectedAttribute]));
+    // Round up to the nearest whole number
+    maxY = Math.ceil(maxY);
+
     const y = d3
       .scaleLinear()
-      .domain([0, 10]) // Note that here the Y scale is set manually
+      .domain([0, maxY]) // Note that here the Y scale is set manually
       .range([violinHeight - violinMargin.bottom, violinMargin.top]);
     svg
       .append("g")
@@ -92,18 +107,18 @@ d3.csv("data/data_bechdel_newer.csv").then((data) => {
         return filtered.length;
       },
       (d) => d.passed,
-      (d) => d.imdb_rating
+      (d) => d[selectedAttribute]
     );
 
     // Sort by sum
     sortedPass = new Map(
       [...sumstat.get("Pass")].sort((a, b) => {
-        return a[0].localeCompare(b[0]);
+        return a[0].localeCompare(b[0], "en", { numeric: true });
       })
     );
     sortedFail = new Map(
       [...sumstat.get("Fail")].sort((a, b) => {
-        return a[0].localeCompare(b[0]);
+        return a[0].localeCompare(b[0], "en", { numeric: true });
       })
     );
     sumstat.set("Pass", sortedPass);
@@ -122,7 +137,7 @@ d3.csv("data/data_bechdel_newer.csv").then((data) => {
       .range([0, x.bandwidth()])
       .domain([-maxNum, maxNum]);
 
-    // Add the shape to this svg!
+    // Add the violin to this svg!
     svg
       .selectAll("myViolin")
       .data(sumstat)
@@ -142,8 +157,63 @@ d3.csv("data/data_bechdel_newer.csv").then((data) => {
           .x0((d) => xNum(-d[1]))
           .x1((d) => xNum(d[1]))
           .y((d) => y(d[0]))
-          .curve(d3.curveCatmullRom) // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
+          .curve(d3.curveNatural) // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
       );
+
+    // Generate the box plots
+    x.domain().forEach((xBand) => {
+      // Compute summary statistics used for the box:
+      let expandedValues = [];
+      sumstat.get(xBand).forEach((v, k) => {
+        expandedValues = expandedValues.concat(Array(v).fill(k));
+      });
+
+      const q1 = d3.quantile(expandedValues, 0.25);
+      const median = d3.quantile(expandedValues, 0.5);
+      const q3 = d3.quantile(expandedValues, 0.75);
+      const interQuantileRange = q3 - q1;
+      const min = q1 - 1.5 * interQuantileRange;
+      const max = q1 + 1.5 * interQuantileRange;
+
+      // Positioning constants
+      const center = x.bandwidth() / 2 + x(xBand);
+      const width = 50;
+
+      // Show the main vertical line
+      svg
+        .append("line")
+        .attr("x1", center)
+        .attr("x2", center)
+        .attr("y1", y(min))
+        .attr("y2", y(max))
+        .attr("stroke", "black");
+
+      // Show the box
+      svg
+        .append("rect")
+        .attr("x", center - width / 2)
+        .attr("y", y(q3))
+        .attr("height", y(q1) - y(q3))
+        .attr("width", width)
+        .attr("stroke", "black")
+        .style("fill", "gray");
+
+      // Show median, min and max horizontal lines
+      svg
+        .selectAll("toto")
+        .data([min, median, max])
+        .enter()
+        .append("line")
+        .attr("x1", center - width / 2)
+        .attr("x2", center + width / 2)
+        .attr("y1", function (d) {
+          return y(d);
+        })
+        .attr("y2", function (d) {
+          return y(d);
+        })
+        .attr("stroke", "black");
+    });
   };
 
   violinPlot();
