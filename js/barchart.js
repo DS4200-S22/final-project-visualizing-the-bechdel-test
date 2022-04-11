3818f7d76f5c0051ed840a36a7a1c8e0c43ed24f
Sisira Mandapaka <sisiramandapaka@gmail.com>
2022-04-11 15:57:23 -0400

    Changed "visualization.js" to "barchart.js" just for clarity's sake.

---
+++ js/barchart.js
@@ -0,0 +1,148 @@
+// Set margins and dimensions
+const margin = { top: 50, right: 50, bottom: 50, left: 200 };
+const width = window.innerWidth; //- margin.left - margin.right;
+const height = 650; //- margin.top - margin.bottom;
+
+// Append SVG object to the body of the page to house bar chart .
+const svg3 = d3
+  .select("#vis-container")
+  .append("svg")
+  .attr("width", width - margin.left - margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
+
+// Color scale
+const color = d3
+  .scaleOrdinal()
+  .range(["#93f9d3", "#5cd8a9", "#35ad7f", "#1f845e"]);
+
+// Plotting
+d3.csv("data/data_bechdel_newer.csv").then((data) => {
+  let genreCounts;
+  let stackFormatted;
+  let stacked;
+
+  // Assign global function accessible to other scripts
+  window.barChart = function (minYear, maxYear) {
+    // Clear existing bar chart
+    svg3.selectAll("*").remove();
+
+    // Create map of counts
+    genreCounts = new Map();
+    stackFormatted = [];
+    data.forEach((d) => {
+      const year = Number.parseInt(d.year);
+      if (year < minYear || year > maxYear) return;
+      const bechdel_rating = d.bechdel_rating;
+      d.genres.split(",").forEach((genre) => {
+        genreCounts.set(genre, genreCounts.get(genre) || new Map());
+        genreCounts
+          .get(genre)
+          .set(
+            bechdel_rating,
+            (genreCounts.get(genre).get(bechdel_rating) || 0) + 1
+          );
+      });
+    });
+
+    // Sort by sum
+    genreCounts = new Map(
+      [...genreCounts].sort(
+        (a, b) =>
+          d3.sum(Array.from(a[1].values())) < d3.sum(Array.from(b[1].values()))
+      )
+    );
+
+    // Format for stacking
+    genreCounts.forEach((v, k) => {
+      stackFormatted.push({
+        genre: k,
+        0: v.get("0") || 0,
+        1: v.get("1") || 0,
+        2: v.get("2") || 0,
+        3: v.get("3") || 0,
+      });
+    });
+
+    stacked = d3.stack().keys(["0", "1", "2", "3"])(stackFormatted);
+
+    // Axis labels
+    const xKey3 = "Genre";
+    const yKey3 = "Number of Movies";
+
+    // Create x scale
+    const x3 = d3
+      .scaleBand()
+      .domain(d3.range(genreCounts.size))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
+
+    // Add x axis
+    svg3
+      .append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(
+        d3.axisBottom(x3).tickFormat((d) => Array.from(genreCounts.keys())[d])
+      )
+      .attr("font-size", "14px")
+      .call((g) =>
+        g
+          .append("text")
+          .attr("x", width / 2)
+          .attr("y", margin.bottom - 4)
+          .attr("fill", "black")
+          .attr("text-anchor", "center")
+          .text(xKey3)
+      );
+
+    // Find max y
+    const maxY3 = d3.max(genreCounts, (d) => {
+      const valuesArray = Array.from(d[1].values());
+      return d3.sum(valuesArray);
+    });
+
+    // Create y scale
+    const y3 = d3
+      .scaleLinear()
+      .domain([0, maxY3])
+      .range([height - margin.bottom, margin.top]);
+
+    // Add y axis
+    svg3
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y3))
+      .attr("font-size", "20px")
+      .call((g) =>
+        g
+          .append("text")
+          .attr("x", 0)
+          .attr("y", margin.top - 10)
+          .attr("fill", "black")
+          .attr("text-anchor", "end")
+          .text(yKey3)
+      );
+
+    // Show the bars
+    svg3
+      .append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stacked)
+      .enter()
+      .append("g")
+      .attr("fill", (d) => color(d.key))
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data((d) => d)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x3(i))
+      .attr("y", (d) => y3(d[1]))
+      .attr("height", (d) => y3(d[0]) - y3(d[1]))
+      .attr("width", x3.bandwidth());
+  };
+
+  // Draw the initial bar chart
+  barChart();
+});
