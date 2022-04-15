c0a6141ea939879929e45490e43452c87d34a771
Blake Heyman <blake.heyman@gmail.com>
2022-04-15 11:08:02 -0400

    Formatting

--- js/violin.js
+++ js/violin.js
@@ -263,22 +263,22 @@ d3.csv("data/data_bechdel_newer.csv").then((data) => {
 
   violinPlot();
 
-   //citation: https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
+  //citation: https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
 
-   const radioButtons = document.querySelectorAll('input[name="distribution"]');
+  const radioButtons = document.querySelectorAll('input[name="distribution"]');
 
-   for (i of radioButtons) {
-     i.addEventListener("click", () => {
-         let selectedDistribution;
-         for (const radioButton of radioButtons) {
-             if (radioButton.checked) {
-               selectedDistribution = radioButton.value;
-                 break;
-             }
-         }
-         // show the output:
-         violinPlot(selectedDistribution, selectedMinYear, selectedMaxYear)
-         //output.innerText = selectedDistribution;
-     });
-   }
+  for (i of radioButtons) {
+    i.addEventListener("click", () => {
+      let selectedDistribution;
+      for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+          selectedDistribution = radioButton.value;
+          break;
+        }
+      }
+      // show the output:
+      violinPlot(selectedDistribution, selectedMinYear, selectedMaxYear);
+      //output.innerText = selectedDistribution;
+    });
+  }
 });
